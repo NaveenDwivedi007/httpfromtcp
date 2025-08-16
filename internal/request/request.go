736a2949d4154920503366f4654b86f06f7241b0
Naveen Dwivedi <nadw@simcorp.com>
2025-08-16 14:54:40 +0530

    start line parser created

--- internal/request/request.go
+++ internal/request/request.go
@@ -1,6 +1,7 @@
 package request
 
 import (
+	"bytes"
 	"fmt"
 	"io"
 	"strings"
@@ -12,30 +13,35 @@ type RequestLine struct {
 	Method        string
 }
 
+type parserState string
+
+const (
+	StateInit parserState = "init"
+	StateDone parserState = "done"
+)
+
 type Request struct {
 	RequestLine RequestLine
+	state       parserState
 }
 
-var ERROR_BAD_START_LINE = fmt.Errorf("start line is bad")
-var SEPARATOR = "\r\n"
+func parseRequestLine(httpRequest []byte) (*RequestLine, int, error) {
 
-func parseRequestLine(httpRequest string) (*RequestLine, string, error) {
-
-	idx := strings.Index(httpRequest, SEPARATOR)
+	idx := bytes.Index(httpRequest, SEPARATOR)
 	if idx == -1 {
-		return nil, httpRequest, ERROR_BAD_START_LINE
+		return nil, 0, nil
 	}
-	startString := httpRequest[:idx]
-	restOfTheString := httpRequest[idx+len(SEPARATOR):]
-
+	startLineByte := httpRequest[:idx]
+	read := idx + len(SEPARATOR)
+	startString := string(startLineByte)
 	req1 := strings.Split(startString, " ")
 	if len(req1) != 3 {
-		return nil, restOfTheString, ERROR_BAD_START_LINE
+		return nil, 0, ERROR_BAD_START_LINE
 	}
 
 	versionPart := strings.Split(req1[2], "/")
 	if len(versionPart) != 2 || versionPart[0] != "HTTP" {
-		return nil, restOfTheString, ERROR_BAD_START_LINE
+		return nil, 0, ERROR_BAD_START_LINE
 	}
 
 	r := &RequestLine{
@@ -43,21 +49,63 @@ func parseRequestLine(httpRequest string) (*RequestLine, string, error) {
 		RequestTarget: req1[1],
 		HttpVersion:   versionPart[1],
 	}
-	return r, restOfTheString, nil
+	return r, read, nil
 }
 
-func RequestFromReader(reader io.Reader) (*Request, error) {
-	n, err := io.ReadAll(reader)
-	if err != nil {
-		return nil, ERROR_BAD_START_LINE
+func (r *Request) parse(data []byte) (int, error) {
+	read := 0
+outer:
+	for {
+		switch r.state {
+		case StateInit:
+			rl, n, err := parseRequestLine(data[read:])
+			if err != nil {
+				return 0, err
+			}
 
-	}
+			if n == 0 {
+				break outer
+			}
 
-	r, _, e := parseRequestLine(string(n))
-	if e != nil {
-		return nil, e
+			r.RequestLine = *rl
+			r.state = StateDone
+			read += n
+		case StateDone:
+			break outer
+		}
 	}
+	return read, nil
+}
+
+func (r *Request) done() bool {
+	return r.state == StateDone
+}
+
+func newRequest() *Request {
 	return &Request{
-		RequestLine: *r,
-	}, e
+		state: StateInit,
+	}
+}
+
+var ERROR_BAD_START_LINE = fmt.Errorf("start line is bad")
+var SEPARATOR = []byte("\r\n")
+
+func RequestFromReader(reader io.Reader) (*Request, error) {
+	request := newRequest()
+	buf := make([]byte, 1024)
+	bufLen := 0
+	for !request.done() {
+		n, err := reader.Read(buf[bufLen:])
+		if err != nil {
+			return nil, ERROR_BAD_START_LINE
+		}
+		bufLen += n
+		readN, err := request.parse(buf[:bufLen])
+		if err != nil {
+			return nil, ERROR_BAD_START_LINE
+		}
+		copy(buf, buf[readN:bufLen])
+		bufLen -= readN
+	}
+	return request, nil
 }
